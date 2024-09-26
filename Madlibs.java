f95f3df93353f585b6349fb0b4da5815af705a2e
Liam Martinez <liam.martinez@student.tamdistrict.org>
2024-09-26 09:54:36 -0700

    Figuring out how to replace

--- Madlibs.java
+++ Madlibs.java
@@ -5,12 +5,23 @@ public class Madlibs
     public static void main (String[] args)
     { 
         Scanner sc = new Scanner(System.in);
+        
+        //MadLibs Strings
+        String madLibs = "I went to the animal <noun>, the <plural noun> and the <number> beasts were there.";
+        // madLibs = "Once upon a time in a <adjective> land, a <noun> <adverb> grew";
+        // madLibs = "I don't like <noun>, and because I wanted to <verb> I ended up in <noun>.";
 
-        System.out.println("What is your name?");
+        System.out.println("Enter a noun");
+        int openingBracketIndex = madLibs.indexOf("<");
+        int closingBracketIndex = madLibs.indexOf(">");
+
+        String madLibsOne = madLibs.substring(openingBracketIndex, closingBracketIndex);
 
         String userInput = sc.nextLine();
 
-        System.out.println("Your name is " + userInput + "!");
+        System.out.println(madLibsOne + userInput);
+
+
 
     }
 }
