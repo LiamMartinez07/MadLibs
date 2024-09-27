3e0fa4bf4e65a0a86a94fd3415f22e3fe68b2764
Liam Martinez <liam.martinez@student.tamdistrict.org>
2024-09-27 08:27:54 -0700

    Code working finally

--- Madlibs.java
+++ Madlibs.java
@@ -8,7 +8,7 @@ public class Madlibs
         
         //MadLibs Strings
         String madLibs = "I went to the animal <noun>, the <plural noun> and the <number> beasts were there.";
-        madLibs = "Once upon a time in a <adjective> land, a <noun> <adverb> grew";
+        madLibs = "Once upon a time in a <adjective> land, a <noun> <adverb> grew.";
         //madLibs = "I don't like <noun>, and because I wanted to <verb (not conjugated)> I ended up in a <noun>.";
 
 
@@ -48,6 +48,5 @@ public class Madlibs
         String entireMadLib = sectionMadLib1 + sectionMadLib2 + sectionMadLib3 + madLibUntilEnd;
 
         System.out.println(entireMadLib);
-
     }
 }
